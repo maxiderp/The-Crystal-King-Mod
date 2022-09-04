e625be4dd3060e1cf20aa6412efa32eb7fd95fc2
MaxiDerp <maxiderp06@gmail.com>
2022-09-04 19:42:12 -0400

    initial commit

---
+++ src/main/java/com/maxiderp/crystalking/crystalKingMod.java
@@ -0,0 +1,10 @@
+package com.maxiderp.crystalking;
+
+import net.fabricmc.api.ModInitializer;
+
+public class crystalKingMod implements ModInitializer {
+    @Override
+    public void onInitialize() {
+        System.out.print("Crystal King Successfully Started");
+    }
+}
